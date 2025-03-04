b2ad9322ac2bc7642ad45528913b9a30c5e5c72f
CrypeDJ <litowrog@gmail.com>
2025-03-04 03:06:23 +0300

    Add Top Bar

--- app/src/main/java/com/crype/entertoapp/presentation/components/ChooseCountryItem.kt
+++ app/src/main/java/com/crype/entertoapp/presentation/components/ChooseCountryItem.kt
@@ -5,11 +5,8 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -49,7 +46,6 @@ fun ChooseCountryItem(
     codeFontWeight: FontWeight,
     countryInfoVerticalPadding: Dp,
     countryInfoHorizontalPadding: Dp,
-
     onClick: () -> Unit
 ) {
     Card(
--- app/src/main/java/com/crype/entertoapp/presentation/screen/ChooseCountryScreen.kt
+++ app/src/main/java/com/crype/entertoapp/presentation/screen/ChooseCountryScreen.kt
@@ -1,14 +1,11 @@
 package com.crype.entertoapp.presentation.screen
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
@@ -37,7 +34,7 @@ fun ChooseCountryScreen(
     val chosenIndex by remember {
         mutableStateOf(1)
     }
-    Column (
+    Column(
         modifier = Modifier
             .fillMaxSize()
     ) {
@@ -62,7 +59,7 @@ fun ChooseCountryScreen(
                 contentPadding = PaddingValues(4.dp),
                 modifier = Modifier.fillMaxWidth()
             ) {
-                itemsIndexed(CountryCodes.entries){index, country ->
+                itemsIndexed(CountryCodes.entries) { index, country ->
                     ChooseCountryItem(
                         isChosen = index == chosenIndex,
                         country = country,
